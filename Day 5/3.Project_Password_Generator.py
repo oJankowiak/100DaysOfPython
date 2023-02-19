3ad6584841040a6b80cd3bc40dd41864c515c599
Aleksandra Jankowiak <aleksandra.jankowiak@opuscapita.com>
2023-02-19 14:05:26 +0100

    Day 5

---
+++ Day 5/3.Project_Password_Generator.py
@@ -0,0 +1,42 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+password = ""
+
+for letter in range(1,nr_letters + 1):
+    number_of_letters = len(letters)
+    random_letter = random.randint(0,number_of_letters-1)
+    letter = letters[random_letter]
+    password += letter
+
+for number in range(0,nr_numbers):
+    number_of_numbers = len(numbers)
+    random_number = random.randint(0,number_of_numbers-1)
+    number = numbers[random_number]
+    password += number
+
+for symbol in range(0,nr_symbols):
+    number_of_symbols = len(symbols)
+    random_symbol = random.randint(0,number_of_symbols-1)
+    symbol = symbols[random_symbol]
+    password += symbol
+
+
+
+print(f"Your pasword is {password}")
+
+
+# for letter in range(0,(nr_letters-1)):
+#     letter = letters[random.randint(0, int(letters))]
+#     print (f"your password is {letter}")
+
