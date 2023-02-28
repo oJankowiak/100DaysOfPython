9b2a10cc26d8157024573a1aa5e0ab88057296a8
Aleksandra Jankowiak <aleksandra.jankowiak@opuscapita.com>
2023-02-28 15:45:34 +0100

    Day 8

---
+++ Day 8/function.py
@@ -0,0 +1,31 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# def greet():
+#     print("Hello")
+#     print("How do you do?")
+#     print("Isn't the weather nice today?")
+
+# greet()
+
+# Function that allows for input
+
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#     print("Isn't the weather nice today?")
+
+# # greet_with_name("Angela")    
+# greet_with_name("Ola")    
+
+# Function with more than 1 input
+
+def greet_with(name1, name2):
+    print(f"Hello {name1}")
+    print(f"Oh Hello {name2}")
+
+# greet_with("Ola", "Angela")
+
+greet_with(name1 = "Ola", name2 = "Angela")
